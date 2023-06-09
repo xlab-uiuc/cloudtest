030109a5f49e793a249b130cb4ea86eb039a54c2
anna-mazhar <ainamazhar998@gmail.com>
2023-06-09 18:13:34 +0500

    Add python grammar and mutation based fuzzer

---
+++ python_fuzzer/fuzz.py
@@ -0,0 +1,82 @@
+from fuzzingbook.Fuzzer import RandomFuzzer
+from fuzzingbook.APIFuzzer import INT_GRAMMAR, FLOAT_GRAMMAR, ASCII_STRING_GRAMMAR, LIST_GRAMMAR
+from fuzzingbook.Grammars import EXPR_GRAMMAR, URL_GRAMMAR
+from fuzzingbook.GeneratorGrammarFuzzer import ProbabilisticGeneratorGrammarFuzzer
+from fuzzingbook.ProbabilisticGrammarFuzzer import ProbabilisticGrammarFuzzer
+import json, random, fuzzingbook, string, sys
+import fuzzingbook.MutationFuzzer as MutationFuzzer
+sys.path.append('../sdk_tests/Azure Storage')
+import driver
+
+
+
+JSON_GRAMMAR = [{"name":"John Doe","age":30,"address":"123 Main Street, Anytown, USA"},{"name":"Jane Doe","age":25,"address":"456 Elm Street, Anytown, USA"},{"name":"Apple","price":100},{"name":"Orange","price":50},{"message":"Hello, World!"}]
+
+
+
+# create main
+if __name__ == '__main__':
+
+    int_fuzzer = ProbabilisticGeneratorGrammarFuzzer(INT_GRAMMAR)
+    float_fuzzer = ProbabilisticGeneratorGrammarFuzzer(FLOAT_GRAMMAR)
+    # string_fuzzer = ProbabilisticGeneratorGrammarFuzzer(ASCII_STRING_GRAMMAR)
+    # url_fuzzer = ProbabilisticGeneratorGrammarFuzzer(URL_GRAMMAR)
+
+    list_fuzzer_int = ProbabilisticGrammarFuzzer(fuzzingbook.APIFuzzer.list_grammar(INT_GRAMMAR))
+    list_fuzzer_float = ProbabilisticGrammarFuzzer(fuzzingbook.APIFuzzer.list_grammar(FLOAT_GRAMMAR))
+    list_fuzzer_string = ProbabilisticGrammarFuzzer(fuzzingbook.APIFuzzer.list_grammar(ASCII_STRING_GRAMMAR))
+
+
+    # load input from file
+    with open('in/input.json', 'r') as f:
+        input_data = f.read()
+
+    # todo: change azure storage to a variable
+    with open('../sdk_tests/Azure Storage/discrepancy.txt', 'w') as f:
+            f.write('')
+
+    # write to file
+    with open('../sdk_tests/Azure Storage/unique_discrepancy.txt', 'w') as f:
+        f.write('')
+
+    with open('../sdk_tests/Azure Storage/discrepant_methods.txt', 'w') as f:
+        f.write('')
+
+    input_data = json.loads(input_data)
+
+    # iterate through json object and mutate
+    for i in range(1):
+        for key, _ in input_data.items():
+            list_fuzzer = random.choice([list_fuzzer_int, list_fuzzer_float, list_fuzzer_string])
+
+            for i in range(len(input_data[key])):
+                if input_data[key][i] == []:
+                    input_data[key][i] = list(list_fuzzer.fuzz())
+                    continue
+
+                # print(input_data[key])
+                # print(input_data[key][i])
+
+                for j in range(len(input_data[key][i])):
+
+                    # print(type(input_data[key][i][j]).__name__)
+                    # print(input_data[key][i][j])
+                    
+                    if type(input_data[key][i][j]).__name__ == 'int':
+                        input_data[key][i][j] = int_fuzzer.fuzz()
+                    elif type(input_data[key][i][j]).__name__ == 'float':
+                        input_data[key][i][j] = float_fuzzer.fuzz()
+                    elif type(input_data[key][i][j]).__name__ == 'list':
+                        input_data[key][i][j] = list_fuzzer.fuzz()
+                    elif type(input_data[key][i][j]).__name__ == 'dict':
+                        json_fuzz = random.choice(JSON_GRAMMAR)
+                        input_data[key][i][j] = json_fuzz
+                    else:
+                        mutation_fuzzer = MutationFuzzer.MutationFuzzer(seed=[j])
+                        input_data[key][i][j] = mutation_fuzzer.fuzz()
+
+
+    print(input_data)
+
+    
+    driver.main(input_data)
--- sdk_tests/Azure Storage/driver.py
+++ sdk_tests/Azure Storage/driver.py
@@ -49,34 +49,34 @@ def extract_discrepancy(file):
                 li.append(log)
 
     # write to file
-    with open('unique_discrepancy.txt', 'w') as f:
+    with open('../sdk_tests/Azure Storage/unique_discrepancy.txt', 'a') as f:
         f.write('\n\n'.join(li))
 
 
-def run_ops(arg, methods, test_cloud, test_em, t_count, count, buf, discrepant_methods):
+def run_ops(arg, methods, test_cloud, test_em, count, buf, discrepant_methods):
 
+    t_count = -1
     for method in methods:
             t_count += 1
             if method in discrepant_methods:
-                t_count += 1 
+                continue
 
             try:
                 if not method(test_cloud, arg[t_count]) == method(test_em, arg[t_count]) and not method.__name__ in discrepant_methods:
                     count += 1
                     output = buf.getvalue().strip()
 
-                    # empty buffer
-                    buf.truncate(0)
+                    if method.__name__ not in discrepant_methods:
+                        discrepant_methods.append(method.__name__)
 
-                    discrepant_methods.append(method.__name__)
-
-                    with open('discrepancy.txt', 'a') as f:
+                    with open('../sdk_tests/Azure Storage/discrepancy.txt', 'a') as f:
                         f.write(method.__name__ + '\n\n' + output)
                         f.write(f'\n\n\n{count}/{t_count}   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\n\n')
+            
             except Exception as e:
                 print("Exception: ", e)
 
-    return discrepant_methods, t_count, count
+    return count
 
 
 def run_sequences():
@@ -99,9 +99,6 @@ def run_sequences():
     t_count = 0
     list_seqs = list(itertools.combinations(methods, 5))
 
-    with open('discrepancy.txt', 'w') as f:
-        f.write('')
-
     counter = 0
     while counter < len(list_seqs):
         t_count += 1
@@ -116,9 +113,9 @@ def run_sequences():
                     count += 1
                     output = buf.getvalue().strip()
 
-                    with open('discrepancy.txt', 'a') as f:
+                    with open('../sdk_tests/Azure Storage/discrepancy.txt', 'a') as f:
                         f.write('\n'.join(i.__name__ for i in list_seqs[counter]) + '\n\n' + output)
-                        f.write(f'\n\n\n{count}/{t_count}   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\n\n')
+                        f.write(f'\n\n\n{count})   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\n\n')
                     list_seqs = list_seqs[:counter+1] + [list_seqs[i] for i in range(counter, len(list_seqs)) if not check_sublist_in_list(list(list_seqs[i]), sublist)]
                     break
 
@@ -139,75 +136,86 @@ def run1v1(arg):
     methods_queueClient = [getattr(MyQueueClient, attr) for attr in dir(MyQueueClient) if callable(getattr(MyQueueClient, attr)) and not attr.startswith("__")]
     methods_queueServiceClient = [getattr(MyQueueServiceClient, attr) for attr in dir(MyQueueServiceClient) if callable(getattr(MyQueueServiceClient, attr)) and not attr.startswith("__")]
     total_methods = methods_blobClient + methods_containerClient + methods_blobServiceClient + methods_tableClient + methods_tableServiceClient + methods_queueClient + methods_queueServiceClient
+    t_count = len(total_methods)
     # logging.basicConfig(level=logging.DEBUG)
 
+    # empty args for default run of ops
     if arg == ():
-        arg = [()] * len(total_methods)
+        arg = {}
+        arg['1'] = [[]] * len(methods_blobClient)
+        arg['2'] = [[]] * len(methods_containerClient)
+        arg['3'] = [[]] * len(methods_blobServiceClient)
+        arg['4'] = [[]] * len(methods_tableClient)
+        arg['5'] = [[]] * len(methods_tableServiceClient)
+        arg['6'] = [[]] * len(methods_queueClient)
+        arg['7'] = [[]] * len(methods_queueServiceClient)
+    else:
+        assert len(arg) == 7, "Invalid number of clients"
+        assert len(arg['1']) == len(methods_blobClient), "Invalid number of methods for BlobClient"
+        assert len(arg['2']) == len(methods_containerClient), "Invalid number of methods for ContainerClient"
+        assert len(arg['3']) == len(methods_blobServiceClient), "Invalid number of methods for BlobServiceClient"
+        assert len(arg['4']) == len(methods_tableClient), "Invalid number of methods for TableClient"
+        assert len(arg['5']) == len(methods_tableServiceClient), "Invalid number of methods for TableServiceClient"
+        assert len(arg['6']) == len(methods_queueClient), "Invalid number of methods for QueueClient"
+        assert len(arg['7']) == len(methods_queueServiceClient), "Invalid number of methods for QueueServiceClient"
 
-    f = open("discrepant_methods.txt", "w+")
+    # do not run discrepant methods again
+    f = open("../sdk_tests/Azure Storage/discrepant_methods.txt", "r")
     discrepant_methods = f.read().split('\n')
     f.close()
 
-    # test_cloud_bc = BlobClient(False)
-    # test_em_bc = BlobClient()
-    # test_cloud_cc = ContainerClient(False)
-    # test_em_cc = ContainerClient()
-    # test_cloud_bsc = MyBlobServiceClient(False)
-    # test_em_bsc = MyBlobServiceClient()
-    # test_cloud_tc = MyTableClient(False)
-    # test_em_tc = MyTableClient()
-    # test_cloud_tsc = MyTableServiceClient(False)
-    # test_em_tsc = MyTableServiceClient()
+    test_cloud_bc = BlobClient(False)
+    test_em_bc = BlobClient()
+    test_cloud_cc = ContainerClient(False)
+    test_em_cc = ContainerClient()
+    test_cloud_bsc = MyBlobServiceClient(False)
+    test_em_bsc = MyBlobServiceClient()
+    test_cloud_tc = MyTableClient(False)
+    test_em_tc = MyTableClient()
+    test_cloud_tsc = MyTableServiceClient(False)
+    test_em_tsc = MyTableServiceClient()
     test_cloud_qc = MyQueueClient(False)
     test_em_qc = MyQueueClient()
     test_cloud_qsc = MyQueueServiceClient(False)
     test_em_qsc = MyQueueServiceClient()
 
-    count = 0
-    t_count = -1
-    
-    with open('discrepancy.txt', 'w') as f:
-        f.write('')
+    d_count = 0
 
+
+    # to-do: compare error message when res[0] === false, compare using .response.status_code and response.reason
 
     with io.StringIO() as buf, redirect_stdout(buf):
 
-        # # run blob client ops
-        # discrepant_methods, t_count, count = run_ops(arg, methods_blobClient, test_cloud_bc, test_em_bc, t_count, count, buf, discrepant_methods)
-        # # run container client ops
-        # discrepant_methods, t_count, count = run_ops(arg, methods_containerClient, test_cloud_cc, test_em_cc, t_count, count, buf, discrepant_methods)
-        # # run blob service client ops
-        # discrepant_methods, t_count, count = run_ops(arg, methods_blobServiceClient, test_cloud_bsc, test_em_bsc, t_count, count, buf, discrepant_methods)
-        # # run table client ops
-        # discrepant_methods, t_count, count = run_ops(arg, methods_tableClient, test_cloud_tc, test_em_tc, t_count, count, buf, discrepant_methods)
-        # # run table service client ops
-        # discrepant_methods, t_count, count = run_ops(arg, methods_tableServiceClient, test_cloud_tsc, test_em_tsc, t_count, count, buf, discrepant_methods)
+        # run blob client ops
+        d_count = run_ops(arg["1"], methods_blobClient, test_cloud_bc, test_em_bc, d_count, buf, discrepant_methods)
+        # run container client ops
+        d_count = run_ops(arg["2"], methods_containerClient, test_cloud_cc, test_em_cc, d_count, buf, discrepant_methods)
+        # run blob service client ops
+        d_count = run_ops(arg["3"], methods_blobServiceClient, test_cloud_bsc, test_em_bsc, d_count, buf, discrepant_methods)
+        # run table client ops
+        d_count = run_ops(arg["4"], methods_tableClient, test_cloud_tc, test_em_tc, d_count, buf, discrepant_methods)
+        # run table service client ops
+        d_count = run_ops(arg["5"], methods_tableServiceClient, test_cloud_tsc, test_em_tsc, d_count, buf, discrepant_methods)
         # run queue client ops
-        discrepant_methods, t_count, count = run_ops(arg["0"], methods_queueClient, test_cloud_qc, test_em_qc, t_count, count, buf, discrepant_methods)
+        d_count = run_ops(arg["6"], methods_queueClient, test_cloud_qc, test_em_qc, d_count, buf, discrepant_methods)
         # run queue service client ops
-        discrepant_methods, t_count, count = run_ops(arg["1"], methods_queueServiceClient, test_cloud_qsc, test_em_qsc, t_count, count, buf, discrepant_methods)
+        d_count = run_ops(arg["7"], methods_queueServiceClient, test_cloud_qsc, test_em_qsc, d_count, buf, discrepant_methods)
 
-    with open('discrepancy.txt', 'a') as f:
-        f.write(f'{count}/{t_count}   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-        f.write('Round ended')
+    with open('../sdk_tests/Azure Storage/discrepancy.txt', 'a') as f:
+        f.write(f'{d_count}/{t_count}   ------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+        f.write('Round ended\n')
 
     print(discrepant_methods)
-    with open('discrepant_methods.txt', 'w') as f:
-        f.write("\n".join(discrepant_methods))         
-    print('done')
-    extract_discrepancy('discrepancy.txt')
-    print('done')
+    with open('../sdk_tests/Azure Storage/discrepant_methods.txt', 'w') as f:
+        f.write("\n".join(discrepant_methods))  
+
+    extract_discrepancy('../sdk_tests/Azure Storage/discrepancy.txt')
 
 
 '''test suites'''
 def main(arg):
-    # methods_tableClient = [getattr(MyTableClient, attr) for attr in dir(MyTableClient) if callable(getattr(MyTableClient, attr)) and not attr.startswith("__")]
-    # methods_tableServiceClient = [getattr(MyTableServiceClient, attr) for attr in dir(MyTableServiceClient) if callable(getattr(MyTableServiceClient, attr)) and not attr.startswith("__")]
-    # methods = methods_tableClient + methods_tableServiceClient
-    # print(len(methods))
 
     run1v1(arg)
-    print('done')
     # run_sequences()
 
 
