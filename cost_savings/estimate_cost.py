9e5b643c2023aa241f4554d5831886cb0f797f64
anna-mazhar <ainamazhar998@gmail.com>
2023-09-09 18:40:24 -0500

    Add "popular apis fix vs discrepancies" plots

--- cost_savings/estimate_cost.py
+++ cost_savings/estimate_cost.py
@@ -1,8 +1,13 @@
 import json
 
+from matplotlib import pyplot as plt
+import numpy as np
+
 TOTAL_TESTS = 0
 DISCREPANT_TESTS = 0
+DISCREPANT_METHODS = []
 METHODS_COUNT = {}
+TEST_METHOD_MAP = {}
 
 def get_methods_from_file(file_path):
 
@@ -17,8 +22,8 @@ def find_tests_with_methods(methods_json_path, methods_file_path):
     global DISCREPANT_TESTS
     global TOTAL_TESTS
     global METHODS_COUNT
-
-    methods = get_methods_from_file(methods_file_path)
+    global TEST_METHOD_MAP
+    global DISCREPANT_METHODS
     
     with open(methods_json_path, "r") as f:
         json_data = json.load(f)
@@ -37,18 +42,32 @@ def find_tests_with_methods(methods_json_path, methods_file_path):
         for i in value:
             if i in test_methods_count:
                 test_methods_count[i] += 1
-                METHODS_COUNT[i] += 1
             else:
                 test_methods_count[i] = 1
-                METHODS_COUNT[i] = 1
         for method in methods:
             if method in value:
                 # print(method)
                 if not method in discrepant_methods:
                     discrepant_methods.append(method)
-                tests_with_methods.append(key)
+                if not key in tests_with_methods:
+                    tests_with_methods.append(key)
+
+                if not method in DISCREPANT_METHODS:
+                    DISCREPANT_METHODS.append(method)
+                
+                if not key in TEST_METHOD_MAP:
+                    TEST_METHOD_MAP[key] = [method]
+                else:
+                    TEST_METHOD_MAP[key].append(method)
                 # no need to check for other discrepant methods in this test hence break
-                break
+                # break
+
+    for i in test_methods_count:
+        if i in METHODS_COUNT:
+            METHODS_COUNT[i] += test_methods_count[i]
+        else:
+            METHODS_COUNT[i] = test_methods_count[i]
+
     print(f'Method counts: {json.dumps(test_methods_count, indent=2)}')
     print(f'Unique API methods: {len(test_methods_count)}')
     print(f'Discrepant Unique APIs: {len(discrepant_methods)}')
@@ -88,18 +107,29 @@ def sum_methods_if_test_exists(traffic_json_path, discrepant_test_names):
     return cost
 
 
+# create an api popularity graph against discrepancies
+def create_graph(x, y, x_label, y_label, title):
+    plt.plot(x, y)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    # plt.savefig(file_name)
+    plt.show()
+    
+
 if __name__ == "__main__":
 
-    apps = ['orleans','alpakka','identityazuretable','ironpigeon','sleet','attachmentplugin','snowmaker']
-
+    apps = ['alpakka','orleans','identityazuretable','ironpigeon','sleet','attachmentplugin','snowmaker']
+    methods_file_path = "discrepantApisEmulator.txt"
+    methods = get_methods_from_file(methods_file_path)
+    
     for j in apps:
 
         methods_json_path = f"application_sdk_methods/{j}.json"
-        methods_file_path = "discrepantApisEmulator.txt"
         traffic_json_path = f"application_request_types/{j}.json"
 
         print(f'\nApp: {j} ----- Started\n\n')
-        total_tests, discrepant_tests = find_tests_with_methods(methods_json_path, methods_file_path)
+        total_tests, discrepant_tests = find_tests_with_methods(methods_json_path, methods)
 
         # all tests on the cloud
         cloud_cost = sum_methods_if_test_exists(traffic_json_path, total_tests)
@@ -132,3 +162,41 @@ if __name__ == "__main__":
 
     print("Total tests: ", TOTAL_TESTS)
     print("Potential discrepant tests: ", DISCREPANT_TESTS)
+
+    # sort methods by count
+    METHODS_COUNT = {k: v for k, v in sorted(METHODS_COUNT.items(), key=lambda item: item[1], reverse=True)}
+
+    # print(json.dumps(METHODS_COUNT, indent=2))
+    arr = np.array(list(METHODS_COUNT.values()))
+
+    # x-axis: weighted popularity of methods
+    # ratio = arr/arr.sum() * 100
+    # cumulative_x = np.cumsum(ratio)
+    # x_axis = cumulative_x
+
+
+    # x-axis: all methods used in the tests
+    x_axis = np.arange(1, len(METHODS_COUNT)+1)
+
+
+    # x-axis: only discrepant methods
+    # x_axis = np.arange(0, len(DISCREPANT_METHODS)+1)
+
+    y_axis = []
+    # y_axis.append(len([j for j in TEST_METHOD_MAP.keys() if not TEST_METHOD_MAP[j] == []]))
+    for i in METHODS_COUNT.keys():
+        if i in DISCREPANT_METHODS:
+            for j in TEST_METHOD_MAP.keys():
+                if i in TEST_METHOD_MAP[j]:
+                    TEST_METHOD_MAP[j].pop(TEST_METHOD_MAP[j].index(i))
+
+        y_axis.append(len([j for j in TEST_METHOD_MAP.keys() if not TEST_METHOD_MAP[j] == []]))
+    
+    y_axis = np.array(y_axis)
+    # scale between 0 and 100
+    y_axis = ((y_axis - y_axis.min())/(y_axis.max() - y_axis.min())) * 100
+    create_graph(x_axis, y_axis, "Fixed Discrepant APIs (Descending Order of Popularity)", "Test Discrepancies", "")
+
+    
+    
+        
