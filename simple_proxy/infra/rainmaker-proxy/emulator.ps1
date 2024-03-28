ab06d0306448ec2a99c959d7ac75da0945dd6634
anna-mazhar <ainamazhar998@gmail.com>
2024-03-28 16:44:12 -0500

    Delete unwanted stuff

--- simple_proxy/infra/rainmaker-proxy/emulator.ps1
+++
@@ -1,4 +0,0 @@
-# Clear cache of powershell in order to solve Azurite stuck problems. Unitil now, it works.
-Remove-Variable * -ErrorAction SilentlyContinue; Remove-Module *; $error.Clear(); Clear-Host
-# Start Azurite blob queue and table in three new ports.
-azurite --blobPort 20000 --queuePort 20001 --tablePort 20002
